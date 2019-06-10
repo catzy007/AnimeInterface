c9666aed06c9cc9b68c3a6abfd77bc9a9a8d93a5
catzy007 <catzy.rais@gmail.com>
2019-06-10 22:04:17 +0700

    rewrite modulebatmax

--- moduleBatmax.c
+++ moduleBatmax.c
@@ -1,120 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define boolean unsigned int
 #define true 1
 #define false 0
-#define error -1
 
 //https://cryptii.com/pipes/text-octal
 
-#define MyMax 90 //change this to set maximum battery percentage. default = 90
-#define MyMin 50 //change this to set minimum battery percentage. default = 40
-#define MyOff 30 //change this to set shutdown battery percentage. default = 20
+//you might want to edit this ==========================================
+#define MyMax 95 //change this to set maximum battery percentage. default = 90
+#define MyMin 60 //change this to set minimum battery percentage. default = 40
+#define MyOff 45 //change this to set shutdown battery percentage. default = 20
 
-//some function
-int MyBatPrecMax(int max);
-int MyBatPrecMin(int min);
-int MyBatState();
+//batmax function ======================================================
+int getBattPerc(void); //this function will return current battery percentage
+boolean getStatusCharge(void); //this fucntion will return true if battery is charged
 
-//main function
+//main function ========================================================
 void main(){
-	if(MyBatState()==0 && MyBatPrecMin(MyOff)){
-		//if laptop is disharged and battery less than off
-		system("poweroff");
-	}else if(MyBatState() && MyBatPrecMax(MyMax)){
-		//if laptop is charged and battery more than max
-		printf("AI:Unplug|Charger|Now!\n");
-	}else if(MyBatState()==0 && MyBatPrecMin(MyMin)){
-		//if laptop is discharged and battery less than min
+	int status = 0;
+	
+//if laptop is discharged and battery under MyOff ----------------------
+	if(getStatusCharge() == false && getBattPerc() <= MyOff){
+		do{
+			sleep(1);
+			status = system("xterm -e poweroff");
+		}while(status != 0);
+//if laptop is discharged and battery less than MyMin ------------------
+	}else if(getStatusCharge() == false && getBattPerc() <= MyMin){
 		printf("AI:Plug|Charger|Now!\n");
-	}else if(MyBatState()){
-		printf("Charging. Max:%d min:%d off:%d\n",MyMax,MyMin,MyOff);
+//if laptop is discharged and battery at safe level --------------------
+	}else if(getStatusCharge() == false){
+		printf("discharging. Curr:%d%% Max:%d%% Min:%d%% Off:%d%%\n",
+			getBattPerc(),MyMax,MyMin,MyOff);
+//if laptop is charged and battery more than MyMax ---------------------
+	}else if(getStatusCharge() == true && getBattPerc() >= MyMax){
+		printf("AI:Unplug|Charger|Now!\n");
+//if laptop is charged and battery at safe level -----------------------
 	}else{
-		printf("Discharging. Max:%d min:%d off:%d\n",MyMax,MyMin,MyOff);
+		printf("Charging. Cur:%d%% Max:%d%% Min:%d%% Off:%d%%\n",
+			getBattPerc(), MyMax, MyMin, MyOff);
 	}
 }
 
-//if battery percentage more than equ max
-int MyBatPrecMax(int max){
-	FILE *fp1;
-	char strperc[6];
-	int i,intperc=0;
-	///fp1 = popen("upower -i $(upower -e | grep 'BAT') | grep percentage | awk '/perc/{print $2}'", "r");
-	///fp1 = popen("\165\160\157\167\145\162\40\55\151\40\44\50\165\160\157\167\145\162\40\55\145\40\174\40\147\162\145\160\40\47\102\101\124\47\51\40\174\40\147\162\145\160\40\160\145\162\143\145\156\164\141\147\145\40\174\40\141\167\153\40\47\57\160\145\162\143\57\173\160\162\151\156\164\40\44\62\175\47", "r");
-	//fp1 = popen("acpi -b | awk '{print $4}'", "r");
-	fp1 = popen("\141\143\160\151\40\55\142\40\174\40\141\167\153\40\47\173\160\162\151\156\164\40\44\64\175\47", "r");
-	//if command failed to run
-	if(fp1==NULL){
-		return -1;
+//this function will return current battery percentage =================
+int getBattPerc(void){
+//some variables -------------------------------------------------------
+	FILE *sysCall;
+	char strTemp[6], *strPerc;
+	int i, intPerc=0;
+	int strPercLen=(sizeof(char)*sizeof(strTemp))-1;
+
+//do system command to check battery percentage ------------------------
+	respool: //if fgets fail <-
+	do{
+		//sysCall = popen("acpi -b | awk '{print $4}'", "r");
+		sysCall = popen("\141\143\160\151\40\55\142\40\174\40\141\167\153\40\47\173\160\162\151\156\164\40\44\64\175\47", "r");
+		sleep(1);
+	}while(sysCall == NULL);
+
+//get and parse the input string ---------------------------------------
+	if((strPerc = fgets(strTemp, strPercLen, sysCall)) == NULL){
+		goto respool; //if fgets fail <-
 	}
-	fgets(strperc,sizeof(strperc)-1,fp1);
-	pclose(fp1);
-	for(i=0;i<sizeof(strperc);i++){
-		if(strperc[i]=='\045' || strperc[i]=='\54'){
-			strperc[i]='\040';
+	pclose(sysCall);
+	
+//filter the input string ----------------------------------------------
+	for(i=0; i<strPercLen; i++){
+		if(strPerc[i]=='\045' || strPerc[i]=='\54'){
+			strPerc[i]='\040';
 		}
 	}
 	//printf("%s",strperc); //debug_line_can_be_removed
-	intperc=atoi(strperc);
-	if(intperc>=max){
-		return 1;
-	}
-	return 0;
+	
+//convert input string to integer and return ---------------------------
+	intPerc=atoi(strPerc);
+	return intPerc;
 }
 
-//if battery percentage less than equ min
-int MyBatPrecMin(int min){
-	FILE *fp1;
-	char strperc[6];
-	int i,intperc=0;
-	///fp1 = popen("upower -i $(upower -e | grep 'BAT') | grep percentage | awk '/perc/{print $2}'", "r");
-	///fp1 = popen("\165\160\157\167\145\162\40\55\151\40\44\50\165\160\157\167\145\162\40\55\145\40\174\40\147\162\145\160\40\47\102\101\124\47\51\40\174\40\147\162\145\160\40\160\145\162\143\145\156\164\141\147\145\40\174\40\141\167\153\40\47\57\160\145\162\143\57\173\160\162\151\156\164\40\44\62\175\47", "r");
-	//fp1 = popen("acpi -b | awk '{print $4}'", "r");
-	fp1 = popen("\141\143\160\151\40\55\142\40\174\40\141\167\153\40\47\173\160\162\151\156\164\40\44\64\175\47", "r");
-	//if command failed to run
-	if(fp1==NULL){
-		return -1;
-	}
-	fgets(strperc,sizeof(strperc)-1,fp1);
-	pclose(fp1);
-	for(i=0;i<sizeof(strperc);i++){
-		if(strperc[i]=='\045' || strperc[i]=='\54'){
-			strperc[i]='\040';
-		}
-	}
-	intperc=atoi(strperc);
-	if(intperc<=min){
-		return 1;
-	}
-	return 0;
-}
+//this fucntion will return true if battery is charged =================
+boolean getStatusCharge(void){
+//some variables -------------------------------------------------------
+	FILE *sysCall;
+	int i, mark=0;
+	char strTemp[16], *strState;
+	char disState[]="Charging,";
+	int strStateLen=(sizeof(char)*sizeof(strTemp))-1;
+	int strDisLen=(sizeof(char)*sizeof(disState))-1;
 
-//if battery state charging or discharging
-int MyBatState(void){
-	FILE *fp2;
-	int i,mark=0;
-	char strstate[16];
-	char state[]="Discharging,";
-	///fp2 = popen("upower -i $(upower -e | grep 'BAT') | grep state | awk '/state/{print $2}'", "r");
-	///fp2 = popen("\165\160\157\167\145\162\40\55\151\40\44\50\165\160\157\167\145\162\40\55\145\40\174\40\147\162\145\160\40\47\102\101\124\47\51\40\174\40\147\162\145\160\40\163\164\141\164\145\40\174\40\141\167\153\40\47\57\163\164\141\164\145\57\173\160\162\151\156\164\40\44\62\175\47", "r");
-	//fp2 = popen("acpi -b | awk '{print $3}'", "r");
-	fp2 = popen("\141\143\160\151\40\55\142\40\174\40\141\167\153\40\47\173\160\162\151\156\164\40\44\63\175\47", "r");
-	//if command failed to run
-	if(fp2==NULL){
-		return -1;
+//do system command to check battery status ----------------------------
+	respool: //if fgets fail <-
+	do{
+		//sysCall = popen("acpi -b | awk '{print $3}'", "r");
+		sysCall = popen("\141\143\160\151\40\55\142\40\174\40\141\167\153\40\47\173\160\162\151\156\164\40\44\63\175\47", "r");
+		sleep(1);
+	}while(sysCall == NULL);
+
+//get and parse the input string ---------------------------------------
+	if((strState = fgets(strTemp, strStateLen, sysCall)) == NULL){
+		goto respool; //if fgets fail <-
 	}
-	fgets(strstate,sizeof(strstate)-1,fp2);
-	pclose(fp2);
+	pclose(sysCall);
+
+//comparator algoritm --------------------------------------------------
 	mark=0;
-	for(i=0;i<sizeof(state);i++){
-		if(strstate[i]==state[i]){
+	for(i=0; i<strDisLen; i++){
+		if(strState[i] == disState[i]){
 			mark+=1;
 		}
 	}
-	//printf("%s %s %d %d\n",strstate,state,mark,sizeof(state)-1);
-	if(mark==sizeof(state)-1){
-		return 0;
+	//printf("%s %s %d %d\n",strstate,state,mark,sizeof(state)-1); //debug_line_can_be_removed
+	if(mark == strDisLen){
+		return true;
 	}
-	return 1;
+	return false;
 }
