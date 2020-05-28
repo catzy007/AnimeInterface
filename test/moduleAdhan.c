d24cff781a40ba54d39e9c68159359afcec6853a
catzy007 <catzy.rais@gmail.com>
2020-05-28 21:00:39 +0700

    update converter

--- test/moduleAdhan.c
+++ test/moduleAdhan.c
@@ -3,6 +3,8 @@
 #include <time.h> 
 #include <string.h>
 
+int timeFormatToMin(const char *jsonObjectTime);
+
 int main(int argc, char **argv) {
 	FILE *fp;
 	char buffer[2048];
@@ -16,7 +18,7 @@ int main(int argc, char **argv) {
 	struct json_object *Asr;
 	struct json_object *Maghrib;
 	struct json_object *Isha;
-
+	
 	system("curl \"http://api.aladhan.com/v1/timingsByCity?city=semarang&country=ID&method=8\" > timing.json");
 
 	fp = fopen("timing.json","r");
@@ -37,16 +39,26 @@ int main(int argc, char **argv) {
 		json_object_object_get_ex(timings, "Maghrib", &Maghrib);
 		json_object_object_get_ex(timings, "Isha", &Isha);
 
-		printf("Fajr: %s\n", json_object_get_string(Fajr));
-		printf("Dhuhr: %s\n", json_object_get_string(Dhuhr));
-		printf("Asr: %s\n", json_object_get_string(Asr));
-		printf("Maghrib: %s\n", json_object_get_string(Maghrib));
-		printf("Isha: %s\n", json_object_get_string(Isha));
+		printf("Fajr: %s - %d\n", json_object_get_string(Fajr), timeFormatToMin(json_object_get_string(Fajr)));
+		printf("Dhuhr: %s - %d\n", json_object_get_string(Dhuhr), timeFormatToMin(json_object_get_string(Dhuhr)));
+		printf("Asr: %s - %d\n", json_object_get_string(Asr), timeFormatToMin(json_object_get_string(Asr)));
+		printf("Maghrib: - %s %d\n", json_object_get_string(Maghrib), timeFormatToMin(json_object_get_string(Maghrib)));
+		printf("Isha: %s - %d\n", json_object_get_string(Isha), timeFormatToMin(json_object_get_string(Isha)));
 
 		time_t now = time (0);
 		strftime (buff, 100, "%H:%M", localtime (&now));
+		printf("Now: %s - %d\n", buff, timeFormatToMin(buff));
 	}
-    printf ("Now: %s\n", buff);
-
+    
 	return 0;
+}
+
+int timeFormatToMin(const char *formattedTime){
+	char tHour[3];
+	char tMinu[3];
+	tHour[0]=formattedTime[0]; tHour[1]=formattedTime[1];
+	tHour[2]='\0';
+	tMinu[0]=formattedTime[3]; tMinu[1]=formattedTime[4];
+	tMinu[2]='\0';
+	return (atoi(tHour)*60) + atoi(tMinu);
 }
